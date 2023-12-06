8f8766e99d74817704018de1cc36d82c25883027
Leonardo Coreixas <lcoreixas@provertec.com.br>
2023-12-06 00:31:38 -0300

    fix: add field

--- src/pages/admin/Jogar/index.tsx
+++ src/pages/admin/Jogar/index.tsx
@@ -1,17 +1,73 @@
+import { useEffect, useState } from "react";
 import Typography from "../../../components/atoms/Typography";
+import "./index.css"; 
+import { Button, Spinner } from "@material-tailwind/react"; 
 
-function Jogar() {
+function App() {
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <>
+  const playGame = () => {
+    setLoading(false);
+    setTimeout(() => {
+      setLoading(true);
+    }, 5000);
+  };
+  return (
+    <div className="App">
+      <center>
+        <div className="flex px-10 pt-14 pb-8 border-b-[1px] border-gray-200">
+          <Typography className="w-full" variant="h4" tag="h4" color="gray-800">
+            Jogar
+          </Typography>
+        </div>
+        <body>
+          <Button onClick={playGame} style={{ marginTop: "10px" }}>
+            Jogar
+          </Button>
 
-            <div className="flex px-10 pt-14 pb-8 border-b-[1px] border-gray-200">
-                <Typography className="w-full" variant="h4" tag='h4' color="gray-800">Jogar</Typography>
+          {loading ? (
+            <div className="hero">
+              <div className="highway">
+                <div className="city">
+                  <div className="car_example_1">
+                    <img
+                      src="../../src/assets/images/image_1.png"
+                      alt="car_example"
+                    />
+                  </div>
+                  <div className="car_example_2">
+                    <img
+                      src="../../src/assets/images/image_2.png"
+                      alt="car_example"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-           
-
-        </>
-    );
+          ) : (
+            <div className="hero">
+              <div className="highway">
+                <div className="city">
+                  <div className="car_1">
+                    <img
+                      src="../../src/assets/images/image_1.png"
+                      alt="car_1"
+                    />
+                  </div>
+                  <div className="car_2">
+                    <img
+                      src="../../src/assets/images/image_2.png"
+                      alt="car_2"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </body>
+      </center>
+    </div>
+  );
 }
 
-export default Jogar;
+export default App;
