06a30fed83e5a46e98bac309c94b1614278511ca
Leonardo Coreixas <lcoreixas@provertec.com.br>
2023-12-11 22:03:40 -0300

    fix: voucher

--- src/components/cartesi/withdraw/index.tsx
+++ src/components/cartesi/withdraw/index.tsx
@@ -1,48 +1,71 @@
 // import { BigNumber, ethers } from "ethers";
-// import React, { useEffect } from "react";
-// import { useVouchersQuery, useVoucherQuery } from "../helpers/generated/index";
-// import { useRollups } from "../helpers/useRollups";
+// import React, { useEffect, useState } from "react";
+// import { useRollups } from "./useRollups";
+// import { gql } from "@apollo/client";
+// import { useVouchersQuery, useVoucherQuery } from "./generated/index";
+// import {
+//     Dialog,
+//     DialogTitle,
+//     DialogContent,
+//     DialogActions,
+//     Button,
+//     Table,
+//     TableHead,
+//     TableBody,
+//     TableRow,
+//     TableCell,
+//     Grid,
+//     CircularProgress 
+// } from '@mui/material';
 
-// type Voucher = {
-//     id: string;
-//     index: number;
-//     destination: string;
-//     input: any, //{index: number; epoch: {index: number; }
-//     payload: string;
-//     proof: any;
-//     executed: any;
-// };
 
-// interface IVoucherPropos {
-//     dappAddress: string 
-// }
-
-// export const Vouchers: React.FC<IVoucherPropos> = (propos) => {
-//     const [result,reexecuteQuery] = useVouchersQuery();
-//     const [voucherToFetch, setVoucherToFetch] = React.useState([0,0]);
-//     const [voucherResult,reexecuteVoucherQuery] = useVoucherQuery({
-//         variables: { voucherIndex: voucherToFetch[0], inputIndex: voucherToFetch[1] }//, pause: !!voucherIdToFetch
+// export const Vouchers = (propos) => {
+//     const [voucherToFetch, setVoucherToFetch] = useState([0, 0]);
+//     const [openModal, setOpenModal] = useState(false);
+//     const [result, reexecuteQuery] = useVouchersQuery();
+//     const [loading, setLoading] = useState(false);
+//     const [voucherResult, reexecuteVoucherQuery] = useVoucherQuery({
+//         variables: { voucherIndex: voucherToFetch[0], inputIndex: voucherToFetch[1] }
 //     });
-//     const [voucherToExecute, setVoucherToExecute] = React.useState<any>();
+//     debugger
+//     const [voucherToExecute, setVoucherToExecute] = useState();
 //     const { data, fetching, error } = result;
 //     const rollups = useRollups(propos.dappAddress);
 
-//     const getProof = async (voucher: Voucher) => {
-//         setVoucherToFetch([voucher.index,voucher.input.index]);
+//     const getProof = async (voucher) => {
+//         setVoucherToFetch([voucher.index, voucher.input.index]);
 //         reexecuteVoucherQuery({ requestPolicy: 'network-only' });
 //     };
 
-//     const executeVoucher = async (voucher: any) => {
+//     const handleOpenModal = () => {
+//         setOpenModal(true);
+//     };
+
+//     const handleReload = () => {
+//         setLoading(true);
+//         debugger
+//         setTimeout(() => {
+//           reexecuteQuery({ requestPolicy: 'network-only' });
+//           setLoading(false);
+//         }, 2000); 
+    
+//       };
+
+//     const handleCloseModal = () => {
+//         setOpenModal(false);
+//     };
+
+//     const executeVoucher = async (voucher) => {
 //         if (rollups && !!voucher.proof) {
 
-//             const newVoucherToExecute = {...voucher};
+//             const newVoucherToExecute = { ...voucher };
 //             try {
-//                 const tx = await rollups.dappContract.executeVoucher( voucher.destination,voucher.payload,voucher.proof);
+//                 const tx = await rollups.dappContract.executeVoucher(voucher.destination, voucher.payload, voucher.proof);
 //                 const receipt = await tx.wait();
 //                 newVoucherToExecute.msg = `voucher executed! (tx="${tx.hash}")`;
 //                 if (receipt.events) {
 //                     newVoucherToExecute.msg = `${newVoucherToExecute.msg} - resulting events: ${JSON.stringify(receipt.events)}`;
-//                     newVoucherToExecute.executed = await rollups.dappContract.wasVoucherExecuted(BigNumber.from(voucher.input.index),BigNumber.from(voucher.index));
+//                     newVoucherToExecute.executed = await rollups.dappContract.wasVoucherExecuted(BigNumber.from(voucher.input.index), BigNumber.from(voucher.index));
 //                 }
 //             } catch (e) {
 //                 newVoucherToExecute.msg = `COULD NOT EXECUTE VOUCHER: ${JSON.stringify(e)}`;
@@ -51,26 +74,20 @@
 //             setVoucherToExecute(newVoucherToExecute);
 //         }
 //     }
-
-//     useEffect( () => {
-//         const setVoucher = async (voucher: any) => {
+//     useEffect(() => {
+//         const setVoucher = async (voucher) => {
 //             if (rollups) {
-//                 voucher.executed = await rollups.dappContract.wasVoucherExecuted(BigNumber.from(voucher.input.index),BigNumber.from(voucher.index));
+//                 voucher.executed = await rollups.dappContract.wasVoucherExecuted(BigNumber.from(voucher.input.index), BigNumber.from(voucher.index));
 //             }
 //             setVoucherToExecute(voucher);
 //         }
-    
-//         if (!voucherResult.fetching && voucherResult.data){
+
+//         if (!voucherResult.fetching && voucherResult.data) {
 //             setVoucher(voucherResult.data.voucher);
 //         }
-//     },[voucherResult, rollups]);
-
-//     if (fetching) return <p>Loading...</p>;
-//     if (error) return <p>Oh no... {error.message}</p>;
-
-//     if (!data || !data.vouchers) return <p>No vouchers</p>;
-
-//     const vouchers: Voucher[] = data.vouchers.edges.map((node: any) => {
+//     }, [voucherResult, rollups]);
+//     debugger
+//     const vouchers = data?.vouchers?.edges.map((node) => {
 //         const n = node.node;
 //         let payload = n?.payload;
 //         let inputPayload = n?.input.payload;
@@ -85,48 +102,48 @@
 //         }
 //         if (payload) {
 //             const decoder = new ethers.utils.AbiCoder();
-//             const selector = decoder.decode(["bytes4"], payload)[0]; 
-//             payload = ethers.utils.hexDataSlice(payload,4);
+//             const selector = decoder.decode(["bytes4"], payload)[0];
+//             payload = ethers.utils.hexDataSlice(payload, 4);
 //             try {
-//                 switch(selector) { 
-//                     case '0xa9059cbb': { 
+//                 switch (selector) {
+//                     case '0xa9059cbb': {
 //                         // erc20 transfer; 
-//                         const decode = decoder.decode(["address","uint256"], payload);
+//                         const decode = decoder.decode(["address", "uint256"], payload);
 //                         payload = `Erc20 Transfer - Amount: ${ethers.utils.formatEther(decode[1])} - Address: ${decode[0]}`;
-//                         break; 
+//                         break;
 //                     }
-//                     case '0x42842e0e': { 
+//                     case '0x42842e0e': {
 //                         //erc721 safe transfer;
-//                         const decode = decoder.decode(["address","address","uint256"], payload);
+//                         const decode = decoder.decode(["address", "address", "uint256"], payload);
 //                         payload = `Erc721 Transfer - Id: ${decode[2]} - Address: ${decode[1]}`;
-//                         break; 
+//                         break;
 //                     }
-//                     case '0x522f6815': { 
+//                     case '0x522f6815': {
 //                         //ether transfer; 
 //                         const decode2 = decoder.decode(["address", "uint256"], payload)
 //                         payload = `Ether Transfer - Amount: ${ethers.utils.formatEther(decode2[1])} (Native eth) - Address: ${decode2[0]}`;
-//                         break; 
+//                         break;
 //                     }
-//                     case '0xd0def521': { 
+//                     case '0xd0def521': {
 //                         //erc721 mint;
-//                         const decode = decoder.decode(["address","string"], payload);
+//                         const decode = decoder.decode(["address", "string"], payload);
 //                         payload = `Mint Erc721 - String: ${decode[1]} - Address: ${decode[0]}`;
-//                         break; 
+//                         break;
 //                     }
-//                     case '0x755edd17': { 
+//                     case '0x755edd17': {
 //                         //erc721 mintTo;
 //                         const decode = decoder.decode(["address"], payload);
 //                         payload = `Mint Erc721 - Address: ${decode[0]}`;
-//                         break; 
+//                         break;
 //                     }
-//                     case '0x6a627842': { 
+//                     case '0x6a627842': {
 //                         //erc721 mint;
 //                         const decode = decoder.decode(["address"], payload);
 //                         payload = `Mint Erc721 - Address: ${decode[0]}`;
-//                         break; 
+//                         break;
 //                     }
 //                     default: {
-//                         break; 
+//                         break;
 //                     }
 //                 }
 //             } catch (e) {
@@ -140,89 +157,70 @@
 //             index: parseInt(n?.index),
 //             destination: `${n?.destination ?? ""}`,
 //             payload: `${payload}`,
-//             input: n ? {index:n.input.index,payload: inputPayload} : {},
+//             input: n ? { index: n.input.index, payload: inputPayload } : {},
 //             proof: null,
 //             executed: null,
 //         };
-//     }).sort((b: any, a: any) => {
+//     }).sort((b, a) => {
 //         if (a.input.index === b.input.index) {
 //             return b.index - a.index;
 //         } else {
 //             return b.input.index - a.input.index;
 //         }
-//     });
+//     }) ?? [];
 
-//     // const forceUpdate = useForceUpdate();
 //     return (
 //         <div>
-//             <p>Voucher to execute</p>
-//         {voucherToExecute ? <table>
-//             <thead>
-//                 <tr>
-//                     <th>Input Index</th>
-//                     <th>Voucher Index</th>
-//                     <th>Destination</th>
-//                     <th>Action</th>
-//                     {/* <th>Payload</th> */}
-//                     {/* <th>Proof</th> */}
-//                     <th>Input Payload</th>
-//                     <th>Msg</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 <tr key={`${voucherToExecute.input.index}-${voucherToExecute.index}`}>
-//                     <td>{voucherToExecute.input.index}</td>
-//                     <td>{voucherToExecute.index}</td>
-//                     <td>{voucherToExecute.destination}</td>
-//                     <td>
-//                         <button disabled={!voucherToExecute.proof || voucherToExecute.executed} onClick={() => executeVoucher(voucherToExecute)}>{voucherToExecute.proof ? (voucherToExecute.executed ? "Voucher executed" : "Execute voucher") : "No proof yet"}</button>
-//                     </td>
-//                     {/* <td>{voucherToExecute.payload}</td> */}
-//                     {/* <td>{voucherToExecute.proof}</td> */}
-//                     <td>{voucherToExecute.input.payload}</td>
-//                     <td>{voucherToExecute.msg}</td>
-//                 </tr>
-//             </tbody>
-//         </table> : <p>Nothing yet</p>}
-//             <button onClick={() => reexecuteQuery({ requestPolicy: 'network-only' })}>
-//                 Reload
-//             </button>
-//             <table>
-//                 <thead>
-//                     <tr>
-//                         <th>Input Index</th>
-//                         <th>Voucher Index</th>
-//                         <th>Destination</th>
-//                         <th>Action</th>
-//                         {/* <th>Input Payload</th> */}
-//                         <th>Payload</th>
-//                         {/* <th>Proof</th> */}
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {vouchers.length === 0 && (
-//                         <tr>
-//                             <td colSpan={4}>no vouchers</td>
-//                         </tr>
-//                     )}
-//                     {vouchers.map((n: any) => (
-//                         <tr key={`${n.input.index}-${n.index}`}>
-//                             <td>{n.input.index}</td>
-//                             <td>{n.index}</td>
-//                             <td>{n.destination}</td>
-//                             <td>
-//                                 <button onClick={() => getProof(n)}>Get Proof</button>
-//                             </td>
-//                             {/* <td>{n.input.payload}</td> */}
-//                             <td>{n.payload}</td>
-//                             {/* <td>
-//                                 <button disabled={!!n.proof} onClick={() => executeVoucher(n)}>Execute voucher</button>
-//                             </td> */}
-//                         </tr>
-//                     ))}
-//                 </tbody>
-//             </table>
+//             {/* Your existing JSX */}
+//             <Button onClick={handleOpenModal} style={{ color: "#59a14e" }}>Withdraw</Button>
+//             <Dialog open={openModal} onClose={handleCloseModal}>
+//                 <DialogTitle>Voucher List</DialogTitle>
+//                 <DialogContent>
+//                     <Table>
+//                         <TableHead>
+//                             <TableRow>
+//                                 <TableCell>ID</TableCell>
+//                                 <TableCell>Index</TableCell>
+//                                 <TableCell>Action</TableCell>
+//                             </TableRow>
+//                         </TableHead>
+//                         <TableBody>
+//                             {vouchers.length === 0 && (
+//                                 <TableRow>
+//                                 <TableCell colSpan={2}>
+//                                   <Grid container alignItems="center" justifyContent="center">
+//                                     {loading ? <CircularProgress /> : 'No vouchers found'}
+//                                   </Grid>
+//                                 </TableCell>
+//                                 <TableCell>
+//                                   <Grid container alignItems="center" justifyContent="center">
+//                                     <Button disabled={loading} onClick={() => handleReload()}>
+//                                     {loading ? 'Loading...' : 'Reload'}
+//                                     </Button>
+//                                   </Grid>
+//                                 </TableCell>
+//                               </TableRow>
+//                             )}
+//                             {vouchers.length > 0 && (
+//                                 vouchers.map((voucher) => (
+//                                     <TableRow key={voucher.id}>
+//                                         <TableCell>{voucher.id}</TableCell>
+//                                         <TableCell>{voucher.index}</TableCell>
+//                                         <TableCell>
+//                                             <Button style={{ color: "#fdad00" }} disabled={vouchers.length === 0} onClick={() => executeVoucher(vouchers[0])}>Withdraw</Button>
+//                                         </TableCell>
+//                                     </TableRow>
+//                                 ))
+//                             )}
 
+//                         </TableBody>
+//                     </Table>
+//                 </DialogContent>
+//                 <DialogActions>
+//                     <Button onClick={handleCloseModal}>Close</Button>
+//                 </DialogActions>
+//             </Dialog>
 //         </div>
 //     );
 // };
+
